dcad70dd714bf26a848de9b427f112b7792fb154
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2021-04-22 12:05:11 +0200

    Add docs to AuctioneerStub fallback

--- contracts/test/AuctioneerStub.sol
+++ contracts/test/AuctioneerStub.sol
@@ -5,5 +5,7 @@ pragma solidity <0.9.0;
 import "../Auctioneer.sol";
 
 contract AuctioneerStub is Auctioneer {
+    /// @dev This fallback function is needed by the `impersonateContract`
+    ///      test helper function.
     fallback() external payable {}
 }
