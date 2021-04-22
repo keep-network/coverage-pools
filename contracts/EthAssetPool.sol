25d962a736c0bbe0ae09bebbccd39489e2754442
Tomasz Slabon <tomasz.slabon@keep.network>
2021-04-22 12:07:51 +0200

    Used composition instead of inheritance for eth asset pool

--- contracts/EthAssetPool.sol
+++ contracts/EthAssetPool.sol
@@ -10,40 +10,24 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title EthAssetPool
-/// @notice EthAssetPool extends AssetPool to allow ETH to be used in
+/// @notice EthAssetPool wraps AssetPool to allow ETH to be used in
 ///         coverage-pools
-contract EthAssetPool is AssetPool {
+contract EthAssetPool is Ownable {
     IERC20 wethToken;
+    AssetPool assetPool;
 
-    constructor(address _weth) AssetPool(IERC20(_weth)) {
+    constructor(address _weth) {
         wethToken = IERC20(_weth);
-    }
-
-    /// @notice Accepts the given amount of ETH as a deposit, wraps it in WETH
-    ///         and mints underwriter tokens representing pool's ownership.
-    /// @dev This function is a shortcut for approve + deposit.
-    function receiveApproval(
-        address from,
-        uint256 amount,
-        address token,
-        bytes calldata
-    ) external override {
-        // TODO: implement or remove
-        require(
-            IERC20(token) == collateralToken,
-            "Unsupported collateral token (should be Ether)"
-        );
-
-        super._deposit(from, amount);
+        assetPool = new AssetPool(wethToken);
     }
 
     /// @notice Accepts the given amount of ETH as a deposit, wraps it in WETH
     ///         and mints underwriter tokens representing pool's ownership.
     /// @dev Before calling this function,  needs to have the
     ///      required amount accepted to transfer to the asset pool.
-    function deposit(uint256 amount) public override {
+    function deposit(uint256 amount) public {
         // TODO: implement or remove
-        super.deposit(amount);
+        assetPool.deposit(amount);
     }
 
     /// @notice Withdraws ETH from the asset pool. Accepts the amount of
@@ -52,16 +36,9 @@ contract EthAssetPool is AssetPool {
     ///         caller.
     /// @dev Before calling this function, underwriter token needs to have the
     ///      required amount accepted to transfer to the asset pool.
-    function withdraw(uint256 covAmount) public override {
+    function withdraw(uint256 covAmount) public {
         // TODO: implement or remove
-        super.withdraw(covAmount);
+        assetPool.withdraw(covAmount);
         msg.sender.transfer(covAmount);
-    }
-
-    /// @notice Allows the coverage pool to demand coverage from the asset hold
-    ///         by this pool and send it to the provided recipient address.
-    function claim(address recipient, uint256 amount) public override onlyOwner {
-        // TODO: implement or remove
-        super.claim(recipient, amount);
     }
 }
