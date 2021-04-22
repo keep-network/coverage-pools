d5a90f9f420c043af92a6de198303a295696cc0f
Tomasz Slabon <tomasz.slabon@keep.network>
2021-04-22 14:28:47 +0200

    Implemented deposit and withdraw

--- contracts/EthAssetPool.sol
+++ contracts/EthAssetPool.sol
@@ -5,20 +5,22 @@ pragma solidity <0.9.0;
 import "./AssetPool.sol";
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title EthAssetPool
 /// @notice EthAssetPool wraps AssetPool to allow ETH to be used in
 ///         coverage-pools
 contract EthAssetPool is Ownable {
-    IERC20 public wethToken;
+    address public wethContract;
     AssetPool public assetPool;
 
-    constructor(address _weth) {
-        wethToken = IERC20(_weth);
-        assetPool = new AssetPool(wethToken);
+    constructor(address _wethContract) {
+        // TODO: check if you can cast weth contract to IERC20 if the contract
+        // does not explicitly inherits from the IERC20 (but implements the
+        // necessary functions), like this contract:
+        // https://github.com/gnosis/canonical-weth/blob/master/contracts/WETH9.sol
+        wethContract = _wethContract;
+        assetPool = new AssetPool(IERC20(_wethContract));
     }
 
     /// @notice Accepts the given amount of ETH as a deposit, wraps it in WETH
@@ -26,7 +28,10 @@ contract EthAssetPool is Ownable {
     /// @dev Before calling this function,  needs to have the
     ///      required amount accepted to transfer to the asset pool.
     function deposit(uint256 amount) external payable {
-        // TODO: implement or remove
+        // TODO: check if this function is correct,
+        // add protection agains reentrancy attack
+        (bool success, ) = wethContract.call{value: msg.value}("");
+        require(success, "Failed to send Ether");
         assetPool.deposit(amount);
     }
 
@@ -37,8 +42,10 @@ contract EthAssetPool is Ownable {
     /// @dev Before calling this function, underwriter token needs to have the
     ///      required amount accepted to transfer to the asset pool.
     function withdraw(uint256 covAmount) external {
-        // TODO: implement or remove
+        // TODO: check if this function is correct,
+        // add protection agains reentrancy attack
         assetPool.withdraw(covAmount);
-        msg.sender.transfer(covAmount);
+        (bool success, ) = msg.sender.call{value: covAmount}("");
+        require(success, "Failed to send Ether");
     }
 }
