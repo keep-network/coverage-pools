50997b5081f5d421e73bc66555abb64f14dbd672
Tomasz Slabon <tomasz.slabon@keep.network>
2021-05-24 13:15:23 +0200

    Removed unnecessary check in the auction offer taken flow

--- contracts/RiskManagerV1.sol
+++ contracts/RiskManagerV1.sol
@@ -214,18 +214,13 @@ contract RiskManagerV1 is Auctioneer, Ownable {
         signerBondsSwapStrategy.swapSignerBonds{value: withdrawableAmount}();
     }
 
-    /// @notice Throws if deposit not present among deposits in liquidation or
-    ///         the state of the deposit is not
+    /// @notice Throws if the state of the deposit is not
     ///         DEPOSIT_LIQUIDATION_IN_PROGRESS_STATE.
     /// @dev This function is called when the auctioneer is informed about the
     ///      results of an auction. It is invoked from the Auctioneer
     ///      contract's offerTaken function.
     /// @param auction Address of an auction whose deposit needs to be checked.
     function checkDepositState(address auction) internal override {
-        require(
-            auctionToDeposit[auction] != address(0),
-            "Liquidation not started on deposit"
-        );
         IDeposit deposit = IDeposit(auctionToDeposit[auction]);
         require(
             deposit.currentState() == DEPOSIT_LIQUIDATION_IN_PROGRESS_STATE,
