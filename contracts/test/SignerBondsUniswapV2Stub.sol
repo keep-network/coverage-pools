0a6eae46c0fb44ecde2de071f9bb9a1f95df077e
Piotr Dyraga <piotr.dyraga@keep.network>
2021-06-27 19:59:14 +0200

    Removed unused setUniswapPair function from SignerBondsUniswapV2Stub

--- contracts/test/SignerBondsUniswapV2Stub.sol
+++ contracts/test/SignerBondsUniswapV2Stub.sol
@@ -8,10 +8,4 @@ contract SignerBondsUniswapV2Stub is SignerBondsUniswapV2 {
     constructor(IUniswapV2Router _uniswapRouter, CoveragePool _coveragePool)
         SignerBondsUniswapV2(_uniswapRouter, _coveragePool)
     {}
-
-    /// @dev Meant to be used in tests where there is no possibility to
-    ///      deploy the pair contract at a deterministic address.
-    function setUniswapPair(IUniswapV2Pair _uniswapPair) external {
-        uniswapPair = _uniswapPair;
-    }
 }
