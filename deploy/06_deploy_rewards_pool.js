c13e396fe00ca296760472b8ce55f4d7a9757d25
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-07-02 10:40:11 +0200

    Update RewardsPool deployment script
    The RewardsPool requires 3 args in constructor. See
    c04d11254022ea63050d629262f62ca3d9d873fa.

--- deploy/06_deploy_rewards_pool.js
+++ deploy/06_deploy_rewards_pool.js
@@ -1,12 +1,12 @@
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments
-  const { deployer } = await getNamedAccounts()
+  const { deployer, rewardManager } = await getNamedAccounts()
   const KeepToken = await deployments.get("KeepToken")
   const AssetPool = await deployments.get("AssetPool")
 
   await deploy("RewardsPool", {
     from: deployer,
-    args: [KeepToken.address, AssetPool.address],
+    args: [KeepToken.address, AssetPool.address, rewardManager],
     log: true,
   })
 }
