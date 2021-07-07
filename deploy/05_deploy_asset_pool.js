b1cdc5e0c624b7fc09f975b230ab37ba0af819c7
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-07-07 13:49:09 +0200

    Update the asset pool deployment script
    The `RewardsPool` contract is created in the `AssetPool` constructor.
    With the `06_deploy_rewards_pool.js` script this contract will be
    deployed twice. So to avoid confusion about which contract to use and
    double deployment of the `RewardsPool` contract we can save the
    `RewardsPool` deployment artifact in the` deployments` dir via the
    `save` function from the `hardhat-deploy` plugin.

--- deploy/05_deploy_asset_pool.js
+++ deploy/05_deploy_asset_pool.js
@@ -1,14 +1,39 @@
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { deploy } = deployments
+  const { deploy, getArtifact, log, save } = deployments
   const { deployer, rewardManager } = await getNamedAccounts()
   const KeepToken = await deployments.get("KeepToken")
   const UnderwriterToken = await deployments.get("UnderwriterToken")
+  const RewardsPool = await getArtifact("RewardsPool")
 
-  await deploy("AssetPool", {
+  const AssetPool = await deploy("AssetPool", {
     from: deployer,
     args: [KeepToken.address, UnderwriterToken.address, rewardManager],
     log: true,
   })
+
+  const assetPool = await ethers.getContractAt("AssetPool", AssetPool.address)
+  const rewardsPoolAddress = await assetPool.rewardsPool()
+
+  // The`RewardsPool` contract is created in the `AssetPool` constructor so
+  // it's created in the same transaction.
+  const receipt = AssetPool.receipt
+  receipt.contractAddress = rewardsPoolAddress
+
+  const rewardsPoolDeplymentArtifact = Object.assign(
+    {
+      address: rewardsPoolAddress,
+      receipt,
+      transactionHash: AssetPool.transactionHash,
+    },
+    RewardsPool
+  )
+
+  log("The RewardsPool has been deployed in the same transacion as AssetPool.")
+  log(
+    `The RewardsPool address is: ${rewardsPoolAddress} - saving the RewardsPool deployments info.`
+  )
+
+  await save("RewardsPool", rewardsPoolDeplymentArtifact)
 }
 
 module.exports.tags = ["AssetPool"]
