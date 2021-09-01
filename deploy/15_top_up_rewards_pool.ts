b9273b965cfac9a0137122534c37fe0a488578d8
Rafal Czajkowski <rafal.czajkowski@keep.network>
2021-09-01 12:18:51 +0200

    Add a script that top-ups `RewardsPool` contract
    Just to test coverage-pools on KEEP Token dashboard against Ropsten
    network. NOTE: This is for testing purposes only!!!

---
+++ deploy/15_top_up_rewards_pool.ts
@@ -0,0 +1,48 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { getNamedAccounts, deployments } = hre
+  const { execute } = deployments
+  const { deployer, rewardManager } = await getNamedAccounts()
+
+  const RewardsPool = await deployments.get("RewardsPool")
+
+  const rewardsPoolAddress = RewardsPool.address
+  const reward = "1000000000000000000000000" // 1 000 000
+
+  await execute(
+    "KeepToken",
+    { from: deployer },
+    "approve",
+    rewardManager,
+    reward
+  )
+
+  await execute(
+    "KeepToken",
+    { from: deployer },
+    "transfer",
+    rewardManager,
+    reward
+  )
+
+  await execute(
+    "KeepToken",
+    { from: rewardManager },
+    "approve",
+    rewardsPoolAddress,
+    reward
+  )
+
+  await execute(
+    "RewardsPool",
+    { from: rewardManager },
+    "topUpReward",
+    reward
+  )
+}
+
+export default func
+
+func.tags = ["TopUpRewardsPool"]
