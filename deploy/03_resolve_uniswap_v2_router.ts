4dc116b6363c1333f3e23a9196e430a91b596d81
Dmitry <dymitr.paremski@keep.network>
2021-11-01 15:44:52 +0100

    Prettier formatting

--- deploy/03_resolve_uniswap_v2_router.ts
+++ deploy/03_resolve_uniswap_v2_router.ts
@@ -1,7 +1,7 @@
 import { HardhatRuntimeEnvironment, HardhatNetworkConfig } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
 
-// TODO: THIS DEPLOYMENT CAN BE DONE ONLY IF THERE IS A UNISWAP POOL WITH 
+// TODO: THIS DEPLOYMENT CAN BE DONE ONLY IF THERE IS A UNISWAP POOL WITH
 //       ETH<->T TOKENS.
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { getNamedAccounts, deployments, helpers } = hre
